d885c358bc0a098fca655d93f93d3d1cf58db62e
Jonatas Rodolfo Pereira dos Santos <rodolfostark@ufrn.edu.br>
2022-03-01 14:51:07 -0300

    feat: add Order class and implement a serializer

--- src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -1,19 +1,27 @@
 package br.com.alura.ecommerce;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        try (var dispatcher = new KafkaDispatcher()) {
+        try (var orderDispatcher = new KafkaDispatcher<Order>()) {
             for (var i = 0; i < 10; i++) {
-                var key = UUID.randomUUID().toString();
-                var value = key + ",67523,1234";
-                dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
+                var userId = UUID.randomUUID().toString();
+                var orderId = UUID.randomUUID().toString();
+                var amount = new BigDecimal(Math.random() * 500 + 1);
 
+                var order = new Order(userId, orderId, amount);
+                orderDispatcher.send("ECOMMERCE_NEW_ORDER", userId, order);
+            }
+        }
+        try (var emailDispatcher = new KafkaDispatcher<String>()){
+            for (var i = 0; i < 10; i++) {
+                var userId = UUID.randomUUID().toString();
                 var email = "Thank you for your order! We are processing your order!";
-                dispatcher.send("ECOMMERCE_SEND_EMAIL", key, email);
+                emailDispatcher.send("ECOMMERCE_SEND_EMAIL", userId, email);
             }
         }
     }
