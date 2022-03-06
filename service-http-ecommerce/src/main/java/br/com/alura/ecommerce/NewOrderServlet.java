e867ff076b043f7b01f9b6deeb534df35337cc4d
Jonatas Rodolfo Pereira dos Santos <rodolfostark@ufrn.edu.br>
2022-03-06 15:31:24 -0300

    refactor: changing dispatcher scheme for sent messages

--- service-http-ecommerce/src/main/java/br/com/alura/ecommerce/NewOrderServlet.java
+++ service-http-ecommerce/src/main/java/br/com/alura/ecommerce/NewOrderServlet.java
@@ -1,51 +1,56 @@
 package br.com.alura.ecommerce;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.net.http.HttpResponse;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderServlet extends HttpServlet {
+    private final KafkaDispatcher<Order> orderDispatcher = new KafkaDispatcher<>();
+    private final KafkaDispatcher<Email> emailDispatcher = new KafkaDispatcher<>();
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        orderDispatcher.close();
+        emailDispatcher.close();
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
-        try (var orderDispatcher = new KafkaDispatcher<Order>()) {
-            var email = Math.random() + "@email.com";
-            var userId = UUID.randomUUID().toString();
-            var orderId = UUID.randomUUID().toString();
-            var amount = new BigDecimal(Math.random() * 5000 + 1);
 
-            var order = new Order(orderId, amount, email);
-            try {
-                orderDispatcher.send("ECOMMERCE_NEW_ORDER", email, order);
-                System.out.println("New order sent successfully.");
-                resp.setStatus(HttpServletResponse.SC_OK);
-                resp.getWriter().println("New order sent successfully.");
-            } catch (ExecutionException e) {
-                e.printStackTrace();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        var email = req.getParameter("email");
+        var amount = new BigDecimal(req.getParameter("amount"));
 
+        var orderId = UUID.randomUUID().toString();
+        var order = new Order(orderId, amount, email);
+
+        try {
+            orderDispatcher.send("ECOMMERCE_NEW_ORDER", email, order);
+            System.out.println("New order sent successfully.");
+            resp.setStatus(HttpServletResponse.SC_OK);
+            resp.getWriter().println("New order sent successfully.");
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        try (var emailDispatcher = new KafkaDispatcher<Email>()) {
-            var userId = UUID.randomUUID().toString();
-            var emailSubject = "New order";
-            var emailBody = "Thank you for your order! We are processing your order!";
-            var email = new Email(emailSubject, emailBody);
-            try {
-                emailDispatcher.send("ECOMMERCE_SEND_EMAIL", userId, email);
-            } catch (ExecutionException e) {
-                e.printStackTrace();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+
+        var userId = UUID.randomUUID().toString();
+        var emailSubject = "New order";
+        var emailBody = "Thank you for your order! We are processing your order!";
+        var emailCode = new Email(emailSubject, emailBody);
+        try {
+            emailDispatcher.send("ECOMMERCE_SEND_EMAIL", userId, emailCode);
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
