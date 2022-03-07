a9f6df7aa9c9d9846d395d1da51b944fe8ba1f69
Jonatas Rodolfo Pereira dos Santos <rodolfostark@ufrn.edu.br>
2022-03-06 21:09:06 -0300

    feat: add a report service to the project

---
+++ service-reading-report/src/main/java/ecommerce/ReadingReportService.java
@@ -0,0 +1,51 @@
+package ecommerce;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+import java.math.BigDecimal;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+public class ReadingReportService {
+
+    private final KafkaDispatcher<Order> orderKafkaDispatcher = new KafkaDispatcher<>();
+
+    public static void main(String[] args) {
+        var readingReportService = new ReadingReportService();
+        try (var service = new KafkaService<>(
+                ReadingReportService.class.getSimpleName(),
+                "ECOMMERCE_NEW_ORDER",
+                readingReportService::parse,
+                Order.class,
+                Map.of())) {
+            service.run();
+        }
+    }
+
+    private void parse(ConsumerRecord<String, Order> record) throws ExecutionException, InterruptedException {
+        System.out.println("------------------------------------------");
+        System.out.println("Processing new order, checking for fraud");
+        System.out.println(record.key());
+        System.out.println(record.value());
+        System.out.println(record.partition());
+        System.out.println(record.offset());
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            // ignoring
+            e.printStackTrace();
+        }
+        var order = record.value();
+        if(isFraud(order)) {
+            System.out.println("Order is a fraud!" + order);
+            orderKafkaDispatcher.send("ECOMMERCE_ORDER_REJECTED", order.getEmail(), order);
+        } else {
+            System.out.println("Approved: " + order);
+            orderKafkaDispatcher.send("ECOMMERCE_ORDER_APPROVED", order.getEmail(), order);
+        }
+    }
+
+    private boolean isFraud(Order order) {
+        return order.getAmount().compareTo(new BigDecimal("4500")) >= 0;
+    }
+}
