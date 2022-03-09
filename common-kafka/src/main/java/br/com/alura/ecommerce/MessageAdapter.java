bb0c46460af15f882756cd7f16df46b85477a86f
Jonatas Rodolfo Pereira dos Santos <rodolfostark@ufrn.edu.br>
2022-03-08 21:01:30 -0300

    feat: creating a adapter to built a custom deserializer

--- common-kafka/src/main/java/br/com/alura/ecommerce/MessageAdapter.java
+++ common-kafka/src/main/java/br/com/alura/ecommerce/MessageAdapter.java
@@ -1,13 +1,10 @@
 package br.com.alura.ecommerce;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonSerializationContext;
-import com.google.gson.JsonSerializer;
+import com.google.gson.*;
 
 import java.lang.reflect.Type;
 
-public class MessageAdapter implements JsonSerializer<Message> {
+public class MessageAdapter implements JsonSerializer<Message>, JsonDeserializer<Message> {
     @Override
     public JsonElement serialize(Message message, Type type, JsonSerializationContext jsonSerializationContext) {
         JsonObject obj = new JsonObject();
@@ -15,5 +12,18 @@ public class MessageAdapter implements JsonSerializer<Message> {
         obj.add("payload", jsonSerializationContext.serialize(message.getPayload()));
         obj.add("correlationId", jsonSerializationContext.serialize(message.getCorrelationId()));
         return obj;
+    }
+
+    @Override
+    public Message deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+        var obj = jsonElement.getAsJsonObject();
+        var payloadType = obj.get("type").getAsString();
+        var correlationId = (CorrelationId) jsonDeserializationContext.deserialize(obj.get("correlationId"), CorrelationId.class);
+        try {
+            var payload = jsonDeserializationContext.deserialize(obj.get("payload"), Class.forName(payloadType));
+            return new Message(correlationId, payload);
+        } catch (ClassNotFoundException e) {
+            throw new JsonParseException(e);
+        }
     }
 }
